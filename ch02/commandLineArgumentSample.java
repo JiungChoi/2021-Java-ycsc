3f36a1e132ff0ded6bdacd0aefbb30697739b981
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-09-27 16:11:00 +0900

    Create commandLineArgumentSample.java

---
+++ ch02/commandLineArgumentSample.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    double sum = 0.0;
+    for(int i = 0; i<args.length ; i++){
+      sum += Double.parseDouble(args[i]);
+    }
+    
+    System.out.print("합계 : " +  sum);
+  }
+}
