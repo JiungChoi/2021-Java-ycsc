0fe1f82f2c4ae395e099430d4c99278e8446a6e8
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-09-28 14:41:51 +0900

    Update multiplyNum.java

--- CH03/multiplyNum.java
+++ CH03/multiplyNum.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 class Main4{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("곱하고자 하는 두 수 입력 >> ");
     
-    try{
-      int n = sc.nextInt();
-      int m = sc.nextInt();
-      System.out.print(n +"x" + m + "=" + n*m );
-    }
-    catch(InputMismatchException e){
-      System.out.println(" 실수는 입력하면 안 됩니다.");
-      sc.nextLine();
+    while(true){
+        try{
+          System.out.print("곱하고자 하는 두 수 입력 >> ");
+          int n = sc.nextInt();
+          int m = sc.nextInt();
+          System.out.print(n +"x" + m + "=" + n*m );
+          break;
+        }
+        catch(InputMismatchException e){
+          System.out.println(" 실수는 입력하면 안 됩니다.");
+          sc.nextLine();
+        }
     }
 
   sc.close();
