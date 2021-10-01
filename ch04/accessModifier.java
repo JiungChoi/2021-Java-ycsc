958d27d01b06ed9b7e6d3e5ea1f285a40d9c8f62
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-10-01 11:38:11 +0900

    Create accessModifier.java

---
+++ ch04/accessModifier.java
@@ -0,0 +1,15 @@
+class Sample{
+  public int a;
+  private int b;
+  int c;
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Sample aClass = new Sample();
+    aClass.a = 10;
+    aClass.b = 10; # b에의해 컴파일 안 됨
+    aClass.c = 10;
+    }
+}
+
