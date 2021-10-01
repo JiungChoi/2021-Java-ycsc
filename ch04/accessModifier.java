2ee7888452a134f95cec1dd1c6513179c29acef4
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-10-01 11:48:51 +0900

    Update accessModifier.java

--- ch04/accessModifier.java
+++ ch04/accessModifier.java
@@ -8,7 +8,7 @@ public class Main {
   public static void main(String[] args) {
     Sample aClass = new Sample();
     aClass.a = 10;
-    aClass.b = 10; # b에의해 컴파일 안 됨
+    aClass.b = 10; // b에의해 컴파일 안 됨
     aClass.c = 10;
     }
 }
