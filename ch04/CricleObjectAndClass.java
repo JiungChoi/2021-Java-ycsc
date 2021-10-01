eb69b6af66ff5a715875fafd28ca4a6b1113e850
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-10-01 10:21:43 +0900

    Update CricleObjectAndClass.java

--- ch04/CricleObjectAndClass.java
+++ ch04/CricleObjectAndClass.java
@@ -1,18 +1,20 @@
 public class Main {
 
-  int radius;
-  String name;
+  public static class Circle{
+    int radius;
+    String name;
 
-  public void Circle( ){ }
-
-  public double getAtea(){
-    return 3.14*radius*radius;
+    public double getArea(){
+      return 3.14*radius*radius;
+    }
   }
+
+  
 
   public static void main(String[] args) {
     Circle pizza;
     pizza = new Circle();
-    
+  
     pizza.radius = 10;
     pizza.name = "자바피자";
     double area = pizza.getArea();
@@ -21,7 +23,7 @@ public class Main {
     Circle donut = new Circle();
     donut.radius = 2;
     donut.name = "자바도넛";
-    area = donut.getArea;
+    area = donut.getArea();
     System.out.println(donut.name + "의 면적은" + area);
 
   }
