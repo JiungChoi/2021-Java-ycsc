2deabd2f040cf06c718b3c92eec13e72972bc464
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-10-05 13:49:05 +0900

    Update studentScoreAverage.java

--- CH05/studentScoreAverage.java
+++ CH05/studentScoreAverage.java
@@ -5,16 +5,19 @@ class Main {
     Scanner sc = new Scanner(System.in);
     
     System.out.print("수학, 과학, 영어 순으로 3개의 점수 입력 >>");
-    Grade me = new Grade();
-    me.math = sc.nextInt();
-    me.science = sc.nextInt();
-    me.english = sc.nextInt();
+    
+    int math = sc.nextInt();
+    int science = sc.nextInt();
+    int english = sc.nextInt();
+    Grade me = new Grade(math, science, english);
 
     System.out.println("평균은" +  me.average());
 
     sc.close();
   }
 }
+
+
 
 class Grade{
   static int math;
