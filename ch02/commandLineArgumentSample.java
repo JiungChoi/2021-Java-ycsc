6fc3e451696b3d00f863c6b171b63cb7231cf539
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-10-20 19:47:21 +0900

    Update commandLineArgumentSample.java

--- ch02/commandLineArgumentSample.java
+++ ch02/commandLineArgumentSample.java
@@ -2,7 +2,11 @@ class Main {
   public static void main(String[] args) {
     double sum = 0.0;
     for(int i = 0; i<args.length ; i++){
-      sum += Double.parseDouble(args[i]);
+      try{
+        sum += Double.parseDouble(args[i]);
+      }catch(NumberFormatException e) {
+        continue;
+      }
     }
     
     System.out.print("합계 : " +  sum);
